024737865eb4cd42a4a78a75e8e6865caf0e4b16
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-04 00:16:12 +0100

    Adding the EmailAlert class in Entities

---
+++ NotificationSystem.Core/Entities/EmailAlert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationSystem.Core.Entities
+{
+    public class EmailAlert
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string ReceiverName { get; set; }
+
+        //public UserType Usertype { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateProcessed { get; set; }
+        public bool IsActive { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+        public string CreatedBy { get; set; }
+        public int Sendtries { get; set; }
+    }
+}
