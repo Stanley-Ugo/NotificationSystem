013ac4de3599acd0440669a631b3a381674f2872
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-04 00:00:57 +0100

    Adding the NotificationTemplate class

--- NotificationSystem.Core/Entities/NotificationTemplate.cs
+++ NotificationSystem.Core/Entities/NotificationTemplate.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace NotificationSystem.Core.Entities
 {
-    public class NotificationChannel
+    public class NotificationTemplate
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int NotificationTypeId { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public int NotificationChannelId { get; set; }
+        public NotificationChannel NotificationChannel { get; set; }
         public DateTime DateCreated { get; set; }
         public string CreatedBy { get; set; }
     }
