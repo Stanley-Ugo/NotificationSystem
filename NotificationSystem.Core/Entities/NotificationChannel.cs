5275d6b554c5b05e125f4f79e2ec4f9ef384761b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-03 23:51:04 +0100

    Adding the NotificationChannel class in Entities

---
+++ NotificationSystem.Core/Entities/NotificationChannel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationSystem.Core.Entities
+{
+    class NotificationChannel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
