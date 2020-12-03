d6d70ee2412f60394fe8bab8092b678e9f33b836
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-03 22:31:25 +0100

    Initial Commit - Initialized Project

---
+++ NotificationSystem.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NotificationSystem.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
