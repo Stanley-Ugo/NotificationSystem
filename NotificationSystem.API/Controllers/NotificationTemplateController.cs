1324c47ca672aede4dc15f1c8041f1f4f2758821
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-06 00:34:48 +0100

    Adding the Controllers & View for PostNotification Template

--- NotificationSystem.API/Controllers/NotificationTemplateController.cs
+++ NotificationSystem.API/Controllers/NotificationTemplateController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NotificationSystem.ViewModel.ViewModels.NotificationTemplateVM;
 
 namespace NotificationSystem.API.Controllers
 {
     public class NotificationTemplateController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PostNotification()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostNotification([FromBody] NotificationTemplateFormVM notificationTemplateFormVM)
         {
             return View();
         }
