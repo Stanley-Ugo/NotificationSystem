5029a1115081d2e6af5abb7a4c5102f0a53150f9
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-05 23:56:34 +0100

    Adding NotificationChannelFormVM to ViewModel Project

---
+++ NotificationSystem.ViewModel/ViewModels/NotificationChannelVM/NotificationChannelFormVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NotificationSystem.ViewModel.ViewModels.NotificationChannelVM
+{
+    public class NotificationChannelFormVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
