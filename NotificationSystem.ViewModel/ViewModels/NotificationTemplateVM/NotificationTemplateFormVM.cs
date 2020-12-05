538c398957d1ddd56bfa9400571dd5185e975ba0
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-06 00:27:05 +0100

    Adding NotificationTemplateVM to ViewModel Project

---
+++ NotificationSystem.ViewModel/ViewModels/NotificationTemplateVM/NotificationTemplateFormVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NotificationSystem.ViewModel.ViewModels.NotificationTemplateVM
+{
+    public class NotificationTemplateFormVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
