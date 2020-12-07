7fd51180bbebbd867ceb8a265ef7b51d3c2d816b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-07 01:29:10 +0100

    Finishing up the SMS Alert View

--- NotificationSystem.API/Controllers/NotificationSMSAlertController.cs
+++ NotificationSystem.API/Controllers/NotificationSMSAlertController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NotificationSystem.ViewModel.ViewModels.NotificationSMSAlertVM;
 
 namespace NotificationSystem.API.Controllers
 {
     public class NotificationSMSAlertController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PostNotification()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostNotification([FromBody] NotificationSMSAlertFormVM notificationSMSAlertFormVM)
         {
             return View();
         }
