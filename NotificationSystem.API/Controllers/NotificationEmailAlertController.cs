254d2d09390bc0987d7a4eb09635d71f67959de9
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-07 01:11:26 +0100

    Adding the NotificationEmailAlertFormVM to ViewModel Assembly

--- NotificationSystem.API/Controllers/NotificationEmailAlertController.cs
+++ NotificationSystem.API/Controllers/NotificationEmailAlertController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NotificationSystem.ViewModel.ViewModels.NotificationEmailAlertVM;
 
 namespace NotificationSystem.API.Controllers
 {
     public class NotificationEmailAlertController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PostNotification()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PostNotification([FromBody] NotificationEmailAlertFormVM notificationEmailAlertFormVM)
         {
             return View();
         }
---
+++ NotificationSystem.ViewModel/ViewModels/NotificationEmailAlertVM/NotificationEmailAlertFormVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NotificationSystem.ViewModel.ViewModels.NotificationEmailAlertVM
+{
+    public class NotificationEmailAlertFormVM
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public string ReceiverName { get; set; }
+
+        //public UserType Usertype { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateProcessed { get; set; }
+        public bool IsActive { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+        public string CreatedBy { get; set; }
+        public int Sendtries { get; set; }
+    }
+}
