57f0401b55220e31ef393e1c2ddb31bf3c6ed353
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-09 01:23:13 +0100

    Adding DataTable to NotificationType index Page

--- NotificationSystem.API/Startup.cs
+++ NotificationSystem.API/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,6 +24,8 @@ namespace NotificationSystem.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
