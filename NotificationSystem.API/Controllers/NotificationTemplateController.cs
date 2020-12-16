13c049a749c5d3787ad15c003eedcccfd35fb3e5
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-16 07:24:57 +0100

    Fixing Namespaces in the NotificatiionTemplateController

--- NotificationSystem.API/Controllers/NotificationTemplateController.cs
+++ NotificationSystem.API/Controllers/NotificationTemplateController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NotificationSystem.ViewModel.ViewModels.NotificationTemplateVM;
 
+
 namespace NotificationSystem.API.Controllers
 {
     public class NotificationTemplateController : Controller
