c911aadb6effe9529d88f9fe8c104b94f3ced17b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-16 07:23:08 +0100

    Fixing Namespaces in the NotificatiionEmailAlertController

--- NotificationSystem.API/Controllers/NotificationEmailAlertController.cs
+++ NotificationSystem.API/Controllers/NotificationEmailAlertController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NotificationSystem.ViewModel.ViewModels.NotificationEmailAlertVM;
 
+
 namespace NotificationSystem.API.Controllers
 {
     public class NotificationEmailAlertController : Controller
